aea62b16f11f381b991fed3d19262887182a8624
carloscopes <carlos.mcp.97@gmail.com>
2023-08-05 10:37:17 -0400

    Lógica de JS lista con vínculación hacia el html. Sin modificación de estilos

--- src/app.js
+++ src/app.js
@@ -6,6 +6,36 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let who = ["The dog", "My grandma", "His turtle", "My bird", "Messi"];
+  let action = ["ate", "peed", "crushed", "broke"];
+  let what = ["my homework", "the keys", "the car"];
+  let when = [
+    "before the class",
+    "right on time",
+    "when I finished",
+    "during my lunch",
+    "while I was praying"
+  ];
+
+  function excuseGenerator() {
+    let randomWho = Math.floor(Math.random() * who.length);
+    let randomAction = Math.floor(Math.random() * action.length);
+    let randomWhat = Math.floor(Math.random() * what.length);
+    let randomWhen = Math.floor(Math.random() * when.length);
+    return (
+      who[randomWho] +
+      " " +
+      action[randomAction] +
+      " " +
+      what[randomWhat] +
+      " " +
+      when[randomWhen]
+    );
+  }
+
+  let parrafo = document.getElementById("excuse");
+
+  parrafo.innerHTML = excuseGenerator();
+
+  //Si hago el innerHTML con console.log me da undefined
 };
